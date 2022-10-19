295ff09f2f173c97e901e6cec77758452aa5d55d
Shane Thompson <st83440@gmail.com>
2022-10-19 12:36:13 -0600

    add apod assignment

---
+++ wjs/apod/apod.js
@@ -0,0 +1,55 @@
+const apod = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+
+async function getApod(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  // check to make sure we got a good response
+  if (res.ok) {
+    // success...make sure any previous error message is gone.
+    hideError();
+    return data;
+  } else {
+    // error...output the message returned by the server
+    showError(data.msg);
+    return false;
+  }
+}
+async function getApodByDate() {
+  // get the entered date
+  const date = document.querySelector("#requestedDate");
+  // make the request for the photo with the date
+  const data = await getApod(apod + `&date=${date.value}`);
+  //check to see if we got good results back
+  if (data) {
+    document.querySelector(".output").innerHTML = photoTemplate(data);
+  }
+}
+function showError(msg) {
+  const errorElement = document.querySelector(".error");
+  errorElement.innerHTML = msg;
+  errorElement.classList.remove("hide");
+}
+function hideError() {
+  const errorElement = document.querySelector(".error");
+  errorElement.innerHTML = "";
+  errorElement.classList.add("hide");
+}
+
+function photoTemplate(photo) {
+  return `<section class="photo">
+  <img src=${photo.url} alt="${photo.title}">
+  <div><h2>${photo.title}</h2>
+  <h3>${photo.date}</h3>
+  <p>${photo.explanation}</p></div></section>`;
+}
+async function init() {
+  // get today's image
+  const photo = await getApod(apod);
+  // display it
+  document.querySelector(".output").innerHTML = photoTemplate(photo);
+}
+
+// add an event listener to the Get button.
+document.querySelector("#getButton").addEventListener("click", getApodByDate);
+
+init();
